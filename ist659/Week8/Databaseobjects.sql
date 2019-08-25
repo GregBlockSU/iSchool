3bb244eef9e39e216784f682720d176d6e709452
GregBlockSU <grblock@syr.edu>
2019-08-25 17:40:29 -0400

    Added Week8

---
+++ ist659/Week8/Databaseobjects.sql
@@ -0,0 +1,24 @@
+
+IF OBJECT_ID('InsertFollower') IS NOT NULL
+	DROP PROC InsertFollower;
+go
+
+CREATE PROCEDURE InsertFollower
+	@UserName1 AS varchar(30),
+	@UserName2 AS varchar(30)
+AS
+
+IF NOT EXISTS (SELECT * FROM vc_FollowerList WHERE FollowedID = (SELECT vc_UserID FROM vc_User WHERE UserName = @UserName1) AND
+	FollowerID = (SELECT vc_UserID FROM vc_User WHERE UserName = @UserName2))
+BEGIN
+	INSERT vc_FollowerList (FollowedID, FollowerID, FollowerSince) 
+	VALUES ((SELECT vc_UserID FROM vc_User WHERE UserName = @UserName1), 
+			(SELECT vc_UserID FROM vc_User WHERE UserName = @UserName2),
+			CURRENT_TIMESTAMP );
+END
+	SELECT * FROM vc_FollowerList
+	WHERE FollowedID = (SELECT vc_UserID FROM vc_User WHERE UserName = @UserName1) AND
+			FollowerID = (SELECT vc_UserID FROM vc_User WHERE UserName = @UserName2);
+go
+
+EXEC InsertFollower 'infatuated', 'camel';
